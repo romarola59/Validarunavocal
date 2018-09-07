e7ae7c1a0e0c935c1dd53cea8196603922ab3baa
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-07 20:42:28 -0300

    Agregar archivos de proyecto.

---
+++ validarunavocal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace validarunavocal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
